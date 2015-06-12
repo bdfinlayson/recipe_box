f2f0722d559c6290573e9a6983d2c12d05782f63
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-11 22:07:15 -0500

    Add form for new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,6 +15,11 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Successfully created new recipe'
+    else
+      render 'new'
+    end
   end
 
   private
